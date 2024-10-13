a6930fbab91ea03f4b2d5b77d83c48ee58c67aab
2013o <2013o@DESKTOP-KGS0845>
2024-10-13 22:21:44 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        enum operations
+        {
+            Sum,
+            Dif,
+            Multip,
+            Div,
+            Pow,
+            Sqrt
+        }
+
+        static double DoOperation(operations op, int a, int b)
+        {
+            if (op == operations.Sum)
+            {
+                return a + b;
+            }
+            else if (op == operations.Dif)
+            {
+                return a - b;
+            }
+            else if (op == operations.Multip)
+            {
+                return a * b;
+            }
+            else if (op == operations.Div)
+            {
+                return a / b;
+            }
+            else if (op == operations.Pow)
+            {
+                return Math.Pow(a, b);
+            }
+            else if (op == operations.Sqrt)
+            {
+                return Math.Sqrt(a);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите 2 числа: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            operations op = operations.Sqrt;
+            Console.WriteLine(DoOperation(op, a, b));
+
+
+
+
+        }
+    }
+}
